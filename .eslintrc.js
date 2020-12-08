5029c8348523a47e335df6576cf6a06ddbafe4fb
taiga <taiga.similarities@gmail.com>
2020-12-09 01:44:48 +0900

    add some contents to eslintrc and tsconfig also change package.json

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,22 +1,27 @@
 module.exports = {
   env: {
     browser: true,
-    es2020: true
+    es2020: true,
+    node: true,
+    es6: true
   },
   extends: [
     'plugin:react/recommended',
-    'plugin:@typescript-eslint/recommended'
-    // 'prettier/@typescript-eslint',
-    // 'plugin:prettier/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended'
   ],
+  parserOptions: {
+    project: './tsconfig.json'
+  },
   rules: {
     'no-use-before-define': 'off',
     '@typescript-eslint/no-use-before-define': 'off',
     '@typescript-eslint/no-unused-vars': 'off',
     'no-unused-vars': 'off',
-    // '@typescript-eslint/explicit-module-boundary-types': 'off',
-    // 'explicit-module-boundary-types': 'off',
-
+    '@typescript-eslint/explicit-module-boundary-types': 'off',
+    'explicit-module-boundary-types': 'off'
   },
   parser: '@typescript-eslint/parser',
   parserOptions: {
@@ -27,9 +32,12 @@ module.exports = {
     sourceType: 'module'
   },
   settings: {
-    "react": {
-      "version": "17.0.1"
+    react: {
+      version: '17.0.1'
+    },
+    node: {
+      tryExtensions: ['.js', '.json', '.node']
     }
   },
-  plugins: ['react', '@typescript-eslint']
+  plugins: ['react', '@typescript-eslint', 'react-hooks']
 };
